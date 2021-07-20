c47bf42880a9009d045dd11759ef987458d338fb
raghav-wd <41248639+raghav-wd@users.noreply.github.com>
2021-07-20 21:12:27 +0530

    Initial dependencies setup finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App(nest,) {
   return (
     <div className="App">
       <header className="App-header">
