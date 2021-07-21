8f688488979664d9e947046427cd44257ff714dd
raghav-wd <41248639+raghav-wd@users.noreply.github.com>
2021-07-21 18:57:11 +0530

    Main model rendered

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { Suspense, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Html, useGLTF } from "@react-three/drei";
 
-function App(nest,) {
+function App(nest) {
+  const Model = () => {
+    const gltf = useGLTF("./assets/models/the_magic_room/scene.gltf", true);
+    return <primitive object={gltf.scene} dispose={null} />;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas camera={{ position: [0, 0, 200], fov: 65 }}>
+        <Suspense fallback={null}>
+          <mesh rotation={[0, -Math.PI / 4, 0]} position={[0, -50, 0]}>
+            <Model />
+          </mesh>
+        </Suspense>
+      </Canvas>
     </div>
   );
+}
+
+function Rotate(props) {
+  const ref = useRef();
+  useFrame((state) => (ref.current.rotation.y = state.clock.elapsedTime));
+  return <group ref={ref} {...props} />;
 }
 
 export default App;
